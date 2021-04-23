e8d3f61d1b4a9e644d762c103cdb485d16a2881f
Sanjay <makasanas@yahoo.in>
2021-04-23 20:01:57 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/",
-  // apiUrl: "https://currency-converter-api.webrexstudio.com/",
-  appUrl: "http://localhost:4200",
+  // apiUrl: "http://localhost:3000/",
+  // appUrl: "http://localhost:4200",
+  appUrl: "https://reports.webrexstudio.com",
+  apiUrl: "https://reports-api.webrexstudio.com/",
   appName: "Starter Kit"
 };
 
